dfe4229857efb65fae3d6d71cf805edc713d9010
theamarnath67 <amarnathmaurya778@gmail.com>
2025-08-08 20:44:32 +0530

    Add .next/static/chunks/src_app_privacy_page_tsx_a993527f._.js 1754666072024

---
+++ .next/static/chunks/src_app_privacy_page_tsx_a993527f._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_2f2c70c7._.js"
+],
+    source: "dynamic"
+});
