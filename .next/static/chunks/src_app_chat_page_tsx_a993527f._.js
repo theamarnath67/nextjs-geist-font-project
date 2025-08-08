d60e2bff0b34c2242f99d122788e479d433adae3
theamarnath67 <amarnathmaurya778@gmail.com>
2025-08-08 20:44:21 +0530

    Add .next/static/chunks/src_app_chat_page_tsx_a993527f._.js 1754666060767

---
+++ .next/static/chunks/src_app_chat_page_tsx_a993527f._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_818b6429._.js"
+],
+    source: "dynamic"
+});
